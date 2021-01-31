d6c75847d4d9af5df00d97d86268b83bc52d3778
kierajcullen <kierajcullen@gmail.com>
2021-01-30 21:28:24 -0500

    display current temperature and humidity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
 // Declare Variables
 var APIkey = "d214e05ff0a5e0aef758d2675056c06c";
-var weatherAPI = "https://api.openweathermap.org/data/2.5/weather?";
-var uviAPI = "https://api.openweathermap.org/data/2.5/uvi?lat=";
-var forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?q=";
+// var weatherAPI = "https://api.openweathermap.org/data/2.5/weather?";
+// var uviAPI = "https://api.openweathermap.org/data/2.5/uvi?lat=";
+// var forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?q=";
 // var geoAPI = navigator.geolocation to give access to a location, ask elena if I am implementing this correctly or if this is even necessary;
 var getWeatherIcon = "http://openweathermap.org/img/wn/";
-// var queryURL = ``;
 
-var searchHistoryArr = [];
+var searchHistoryArray = [];
+var forecastHistoryArray = {};
+//when they search a city, add to the
 // console.log("hi);
 // I just realized I made my HTML tags in JS format... I may fix this, I may just note to never do that in the future
 // search for location
 function searchLocation() {
-  //.trim takes away white space
+  //.trim takes away white space... ask elena tonight
   var citySearch = $("#searchInput").val().trim();
   if (citySearch === "") {
     return;
@@ -23,13 +24,14 @@ function searchLocation() {
   createHistory(citySearch);
 }
 
+createHistory();
+
 //call weather when the city is searched
 function getWeather(search) {
   // var queryURL = weatherAPI + "q=" + search + units + APIkey;
   var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${APIkey}&units=imperial`;
   // get the variables back and do an api call to get lat/long... store in variables
   //  api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-
   $.ajax({
     url: queryUrl,
     method: "GET",
@@ -37,6 +39,7 @@ function getWeather(search) {
   }).then(function (response) {
     console.log(response);
     $("#current-city").text(response.name);
+    $("#search-history").text(response.name);
   });
 }
 
@@ -51,8 +54,43 @@ function getForecast(search) {
     //let's do this
   }).then(function (response) {
     console.log(response);
+    $("#temperature").html(
+      "<b>Temperature: </b>" + "" + (response.list[3].main.feels_like + "°F")
+    );
+    $("#humidity").html(
+      "<b>Humidity: </b>" + "" + (response.list[3].main.humidity + "%")
+    );
+    console.log(response.list[3].main.humidity);
+    // $("#date1").text(response.list[3].main);
+    // var results = response;
+    // var date = new Date(results.dt * 1000).toLocaleDateString("en-US");
+    console.log(date);
+    // var forecastSearch = $("#searchInput").val().trim();
+    // var localSearchHistory = JSON.parse(localStorage.getItem("date1"));
+    // var localSearchHistory = getLocalSearchHistory;
+    // $(forecastSearch).text(response.name);
+    // displayForecast();
   });
 }
+
+// function displayForecast(forecast) {
+//   $("#date1").text(response.list[3].main);
+//   var storedForecast = JSON.parse(localStorage.getItem("date1"));
+//   console.log(storedForecast);
+//   if (storedForecast) {
+//     forecastHistoryArray = storedForecast;
+//   }
+//   forecastHistoryArray.push(forecast);
+//   localStorage.setItem("date1", JSON.stringify(forecastHistoryArray));
+// }
+// function createHistory(city) {
+//   var storedCities = JSON.parse(localStorage.getItem("searchHistory"));
+//   if (storedCities) {
+//     searchHistoryArray = storedCities;
+//   }
+//   searchHistoryArray.push(city);
+//   localStorage.setItem("searchHistory", JSON.stringify(searchHistoryArray));
+// }
 
 // search history appears in the searchHistory
 //click handlers
@@ -65,21 +103,23 @@ for (var j = 0; j < 5; j++) {}
 $("#weather-container").show();
 
 // UV index
+// use another .then with an api call
 // set item in local storage then get item
-function displayHistory() {
-  var getLocalSearchHistory = localStorage.getItem("searchHistory");
-  var localSearchHistory = JSON.parse(getLocalSearchHistory);
-
-  if (getLocalSearchHistory === null) {
-    createHistory();
-    getLocalSearchHistory = localStorage.getItem("searchHistory");
-    localSearchHistory = JSON.parse(getLocalSearchHistory);
-  }
-
+function displayHistory(event) {
+  event.preventDefault();
+  console.log(event);
+  var localSearchHistory = JSON.parse(localStorage.getItem("search-history"));
+  // var localSearchHistory = getLocalSearchHistory;
+  $(citySearch).text(response.name);
+  console.log(localSearchHistory);
+  // if (getLocalSearchHistory === null) {
+  //   createHistory();
+  //   getLocalSearchHistory = localStorage.getItem("searchHistory");
+  //   localSearchHistory = JSON.parse(getLocalSearchHistory);
+  // }
   for (var i = 0; i < localSearchHistory.length; i++) {
     // add a list element to display history, local storage
     var historyInfo = $("<li>");
-    historyInfo.addClass("list-group-item");
     historyInfo.text(localSearchHistory[i].city);
     //prepend or append?
     $("#search-history").prepend(historyInfo);
@@ -91,9 +131,14 @@ function displayHistory() {
 
 // Created array in variable section
 // Create History
-function createHistory() {
+function createHistory(city) {
+  var storedCities = JSON.parse(localStorage.getItem("searchHistory"));
+  if (storedCities) {
+    searchHistoryArray = storedCities;
+  }
+  // whatever you search for, add to the empty array, declared in variables
+  searchHistoryArray.push(city);
   // initialize array, store
-  searchHistoryArray.length = 0;
   localStorage.setItem("searchHistory", JSON.stringify(searchHistoryArray));
 }
 
@@ -111,3 +156,9 @@ function clickHistory() {
     getWeather(cityNameHistory);
   });
 }
+
+// calling images
+// trim... again what is the whitespace all about, works without trim... even though there is white space
+// what the heck am i targeting? is it right
+// for loop for every 4th index (index 3), call 12:00 for each day and display this
+// local storage issue... not displaying when the page reloads
