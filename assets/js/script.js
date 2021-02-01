ce8d08cf2edba5f4f6dace841cecaf4439890389
kierajcullen <kierajcullen@gmail.com>
2021-01-31 20:48:23 -0500

    display 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,28 +6,45 @@ var APIkey = "d214e05ff0a5e0aef758d2675056c06c";
 // var geoAPI = navigator.geolocation to give access to a location, ask elena if I am implementing this correctly or if this is even necessary;
 var getWeatherIcon = "http://openweathermap.org/img/wn/";
 
-var searchHistoryArray = [];
+// var searchHistoryArray = [];
+init();
+function init() {
+  $("#cityName-container").hide();
+  // $("#fivedayForecast").hide();
+  $("#currentForecast").hide();
+  $(".card").hide();
+  $("#clearButton").hide();
+}
+// setTimeout(displayHistory(), 2000);
+// setTimeout(function () {
+//   displayHistory();
+//   console.log("alert");
+// }, 3000);
 // var forecastHistoryArray = {};
 //when they search a city, add to the
 // console.log("hi);
 // I just realized I made my HTML tags in JS format... I may fix this, I may just note to never do that in the future
 // search for location
-function searchLocation() {
+function searchLocation(event) {
+  event.preventDefault();
   //.trim takes away white space... ask elena tonight
   var citySearch = $("#searchInput").val().trim();
   if (citySearch === "") {
     return;
   }
+  var searchList = $("<p>").text(citySearch);
+  $("#search-history").prepend(searchList);
+  localStorage.setItem("search-history", citySearch);
   getWeather(citySearch);
   getForecast(citySearch);
   console.log(citySearch);
-  createHistory(citySearch);
 }
-
-createHistory();
 
 //call weather when the city is searched
 function getWeather(search) {
+  // only use .empty if you are creating elements
+  // $("#current-weather-container").empty();
+  // event.preventDefault();
   // var queryURL = weatherAPI + "q=" + search + units + APIkey;
   var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${APIkey}&units=imperial`;
   // get the variables back and do an api call to get lat/long... store in variables
@@ -38,18 +55,28 @@ function getWeather(search) {
     //let's do this
   }).then(function (response) {
     console.log(response);
+    $("#currentForecast").show();
+    $(".card").show();
     $("#current-city").text(response.name);
-    $("#search-history").text(response.name);
+    // var searchList = $("<li>").text(response.name);
+    // $("#search-history").prepend(searchList);
     $("#temperature").html(
       "<b>Temperature: </b>" + "" + (response.main.feels_like + "°F")
     );
     $("#humidity").html(
       "<b>Humidity: </b>" + "" + (response.main.humidity + "%")
     );
+    $("#wind-speed").html(
+      "<b>Wind Speed: </b>" + "" + (response.wind.speed + "MPH")
+    );
     var iconurl =
-      "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
-    $("#weather-image").attr("src", iconurl);
+      "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+
+    $("#current-img").attr("src", iconurl);
+    // clear this out everytime
+    // $("#weather-image-container").attr("src", iconurl);
     // console.log(response.weather[0]);
+    //five day forecast
   });
 }
 
@@ -64,41 +91,46 @@ function getForecast(search) {
     //let's do this
   }).then(function (response) {
     console.log(response);
+    $("#five-day").empty();
+    for (var i = 5; i < 40; i += 8) {
+      // $("#fivedayForecast").show();
+      // var forecastObj = {};
+      var forecastResultsDate = response.list[i].dt_txt;
+      console.log(forecastResultsDate);
+      var forecastDate = new Date(forecastResultsDate).toLocaleDateString(
+        "en-US"
+      );
+      var forecastTemp = response.list[i].main.temp;
+      var forecastHumidity = response.list[i].main.humidity;
 
-    console.log(response.list[3].main.humidity);
-    // $("#date1").text(response.list[3].main);
-    // var results = response;
-    // var date = new Date(results.dt * 1000).toLocaleDateString("en-US");
-    console.log(date);
-    // var forecastSearch = $("#searchInput").val().trim();
-    // var localSearchHistory = JSON.parse(localStorage.getItem("date1"));
-    // var localSearchHistory = getLocalSearchHistory;
-    // $(forecastSearch).text(response.name);
-    // displayForecast();
+      console.log(forecastDate);
+      console.log(forecastTemp);
+      console.log(forecastHumidity);
+      // var fivedayicon =
+      //   "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+
+      // $("#weatherImage1").attr("src", fivedayicon);
+      // console.log(response.weather[0].icon);
+      var dayDiv = $("<div>").addClass("col-lg mx-1");
+      dayDiv.html(`
+      <div class="card text-white bg-primary" id="day1">
+          <div class="card-body">
+            <h5 class="card-title" id="date1">${forecastDate}</h5>
+            <div class="current-temp"><b>Temp: </b> ${forecastTemp}°F
+            <div class="current-humidity"><b>Humidity: </b> ${forecastHumidity}%
+              <img alt="day1-weatherImage" id="weatherImage1"/>
+            </div>
+            <p class="card-text" id="temp1"></p>
+            <p class="card-text" id="humidity1"></p>
+          </div>
+        </div>
+      `);
+
+      $("#five-day").append(dayDiv);
+    }
+    // console.log(response.list[3].main.humidity);
   });
 }
-
-// function displayForecast(forecast) {
-//   $("#date1").text(response.list[3].main);
-//   var storedForecast = JSON.parse(localStorage.getItem("date1"));
-//   console.log(storedForecast);
-//   if (storedForecast) {
-//     forecastHistoryArray = storedForecast;
-//   }
-//   forecastHistoryArray.push(forecast);
-//   localStorage.setItem("date1", JSON.stringify(forecastHistoryArray));
-// }
-// function createHistory(city) {
-//   var storedCities = JSON.parse(localStorage.getItem("searchHistory"));
-//   if (storedCities) {
-//     searchHistoryArray = storedCities;
-//   }
-//   searchHistoryArray.push(city);
-//   localStorage.setItem("searchHistory", JSON.stringify(searchHistoryArray));
-// }
-
-// search history appears in the searchHistory
-//click handlers
 
 $("#searchButton").on("click", searchLocation);
 
@@ -110,57 +142,25 @@ $("#weather-container").show();
 // UV index
 // use another .then with an api call
 // set item in local storage then get item
-function displayHistory(event) {
-  event.preventDefault();
-  console.log(event);
-  var localSearchHistory = JSON.parse(localStorage.getItem("search-history"));
-  // var localSearchHistory = getLocalSearchHistory;
-  $(citySearch).text(response.name);
-  console.log(localSearchHistory);
-  // if (getLocalSearchHistory === null) {
-  //   createHistory();
-  //   getLocalSearchHistory = localStorage.getItem("searchHistory");
-  //   localSearchHistory = JSON.parse(getLocalSearchHistory);
-  // }
-  for (var i = 0; i < localSearchHistory.length; i++) {
-    // add a list element to display history, local storage
-    var historyInfo = $("<li>");
-    historyInfo.text(localSearchHistory[i].city);
-    //prepend or append?
-    $("#search-history").prepend(historyInfo);
-    console.log(historyInfo);
-    $("#search-history-container").show();
+function displayHistory() {
+  var localSearchHistory = localStorage.getItem("search-history");
+  if (localSearchHistory) {
+    getWeather(localSearchHistory);
+    getForecast(localSearchHistory);
   }
-  return (searchHistoryArray = localSearchHistory);
 }
 
-// Created array in variable section
-// Create History
-function createHistory(city) {
-  var storedCities = JSON.parse(localStorage.getItem("searchHistory"));
-  if (storedCities) {
-    searchHistoryArray = storedCities;
-  }
-  // whatever you search for, add to the empty array, declared in variables
-  searchHistoryArray.push(city);
-  // initialize array, store
-  localStorage.setItem("searchHistory", JSON.stringify(searchHistoryArray));
-}
+displayHistory();
 
-function clearHistory() {
-  $("#clear-button").on("click", function () {
-    $("#search-history").empty();
-    localStorage.removeItem("searchHistory");
-    createHistory();
-  });
-}
-
-function clickHistory() {
-  $("#search-history").on("click", "li", function () {
-    var cityNameHistory = $(this).text();
+function clickHistory(event) {
+  if (event.target.matches("p")) {
+    var cityNameHistory = $(event.target).text();
     getWeather(cityNameHistory);
-  });
+  } else {
+    return;
+  }
 }
+$("#search-history").on("click", clickHistory);
 
 // trim... again what is the whitespace all about, works without trim... even though there is white space
 // what the heck am i targeting? is it right
