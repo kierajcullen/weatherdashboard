3f5d2970f293823482df2b247880c29103ada1fd
kierajcullen <kierajcullen@gmail.com>
2021-02-01 17:06:39 -0500

    display current date

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,18 +1,11 @@
-// Declare Variables
+// Declare variables
 var APIkey = "d214e05ff0a5e0aef758d2675056c06c";
-// var weatherAPI = "https://api.openweathermap.org/data/2.5/weather?";
-// var uviAPI = "https://api.openweathermap.org/data/2.5/uvi?lat=";
-// var forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?q=";
-// var geoAPI = navigator.geolocation to give access to a location, ask elena if I am implementing this correctly or if this is even necessary;
 var getWeatherIcon = "http://openweathermap.org/img/wn/";
 
-// var searchHistoryArray = [];
 init();
 function init() {
   $("#cityName-container").hide();
-  // $("#fivedayForecast").hide();
   $("#currentForecast").hide();
-  // $(".card").hide();
   $("#clearButton").hide();
 }
 function searchLocation(event) {
@@ -32,22 +25,15 @@ function searchLocation(event) {
 
 //call weather when the city is searched
 function getWeather(search) {
-  // only use .empty if you are creating elements
-  // $("#current-weather-container").empty();
-  // event.preventDefault();
-  // var queryURL = weatherAPI + "q=" + search + units + APIkey;
   var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${APIkey}&units=imperial`;
-  // get the variables back and do an api call to get lat/long... store in variables
-  //  api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
   $.ajax({
     url: queryUrl,
     method: "GET",
     //let's do this
   }).then(function (response) {
     console.log(response);
-    var date = new Date(response.dt * 1000).toLocaleDateString("en-US");
-    console.log(response.dt_txt);
-    $("#current-date").html(date);
+    var date = new Date(response.dt * 1000).toLocaleDateString('en-US');
+    $("#current-date").text(date);
     $("#currentForecast").show();
     $(".card").show();
     $("#current-city").text(response.name);
@@ -70,12 +56,44 @@ function getWeather(search) {
     //uv index
     var lat = response.coord.lat;
     var lon = response.coord.lon;
-    // clear this out everytime
-    // $("#weather-image-container").attr("src", iconurl);
-    // console.log(response.weather[0]);
-    //five day forecast
-  });
-}
+
+    // get the variables back and do an api call to get lat/long... store in variables
+    // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+    //use lon and lat to get uv data somehow...
+    //loop through and objects, i+=7,
+    // for (var j = 0; j < 5; j++) {
+        
+    // }
+    // color coordinate UV index
+    // var uvi = ....value;
+    $('#uv-index').html(
+      '<b>UV Index: </b>' +
+        '<span id="uv-color">' +
+        uvi +
+        '</span>'
+    );
+    // dynamically add the styling.. like the daily scheduler
+    if (uvi < 3) {
+        $('#uv-color').css('background-color', 'green');
+      } else if (uvi < 6) {
+        $('#uv-color').css('background-color', 'yellow');
+      } else if (uvi < 8) {
+        $('#uv-color').css('background-color', 'orange');
+      } else if (uvi < 11) {
+        $('#uv-color').css('background-color', 'red');
+      } else {
+        $('#uv-color').css('background-color', 'white');
+      }
+    });
+
+    $("#weather-container").show();
+
+    // UV index
+    // use another .then with an api call
+    // set item in local storage then get item
+//   });
+// }
 
 function getForecast(search) {
   // var queryURL = weatherAPI + "q=" + search + units + APIkey;
@@ -88,15 +106,7 @@ function getForecast(search) {
   }).then(function (response) {
     console.log(response);
     $("#five-day").empty();
-    // var currentDate = $("#current-date");
-    // var currentDay = new Date();
-    // var month = months[new Date().getMonth()];
-    // var date = currentDay.getDate();
-    // currentDate.text(day + ", " + month + " " + date + "st");
-
     for (var i = 5; i < 40; i += 8) {
-      // $("#fivedayForecast").show();
-      // var forecastObj = {};
       var forecastResultsDate = response.list[i].dt_txt;
       console.log(forecastResultsDate);
       var forecastDate = new Date(forecastResultsDate).toLocaleDateString(
@@ -108,25 +118,20 @@ function getForecast(search) {
       console.log(forecastDate);
       console.log(forecastTemp);
       console.log(forecastHumidity);
-      // var fivedayicon =
-      //   "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
-
-      // $("#weatherImage1").attr("src", fivedayicon);
-      // console.log(response.weather[0].icon);
       var dayDiv = $("<div>").addClass("col-lg mx-1");
       dayDiv.html(`
-      <div class="card text-white bg-primary" id="day1">
-          <div class="card-body">
-            <h5 class="card-title" id="date1">${forecastDate}</h5>
-            <div class="current-temp"><b>Temp: </b> ${forecastTemp}°F
-            <div class="current-humidity"><b>Humidity: </b> ${forecastHumidity}%
-              <img alt="day1-weatherImage" id="weatherImage1"/>
+        <div class="card text-white bg-primary" id="day1">
+            <div class="card-body">
+              <h5 class="card-title" id="date1">${forecastDate}</h5>
+              <div class="current-temp"><b>Temp: </b> ${forecastTemp}°F
+              <div class="current-humidity"><b>Humidity: </b> ${forecastHumidity}%
+                <img alt="day1-weatherImage" id="weatherImage1"/>
+              </div>
+              <p class="card-text" id="temp1"></p>
+              <p class="card-text" id="humidity1"></p>
             </div>
-            <p class="card-text" id="temp1"></p>
-            <p class="card-text" id="humidity1"></p>
           </div>
-        </div>
-      `);
+        `);
 
       $("#five-day").append(dayDiv);
     }
@@ -136,14 +141,6 @@ function getForecast(search) {
 
 $("#searchButton").on("click", searchLocation);
 
-//use lon and lat to get uv data somehow...
-//loop through and objects, i+=7,
-for (var j = 0; j < 5; j++) {}
-$("#weather-container").show();
-
-// UV index
-// use another .then with an api call
-// set item in local storage then get item
 function displayHistory() {
   var localSearchHistory = localStorage.getItem("search-history");
   if (localSearchHistory) {
